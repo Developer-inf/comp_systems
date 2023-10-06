0c569ec9d804f84d9ce47e8ef0c048c564b3a6ee
Paul <z9099630061@yandex.ru>
2023-10-06 17:54:09 +0300

    Added connection to Database and made json response with data from db to /api/product

--- db/tables/t_product.sql
+++ db/tables/t_product.sql
@@ -1,9 +1,13 @@
+-- drop table t_product;
 create table t_product (
-	id numeric,
-	code varchar,
+	id integer default nextval('"s_product"'::regclass),
+	code varchar not null,
 	prod_name varchar not null,
 	weight numeric not null,
 	description varchar not null
 );
 
 alter table t_product add constraint t_product_pk primary key (id);
+alter table t_product add constraint t_product_uq1 unique (code);
+-- 
+comment on table t_product is 'Table that contains products';
